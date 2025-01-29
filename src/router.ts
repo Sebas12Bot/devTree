bd3e2737a7343ef982d8e93c15f51b0dfd7ffc80
Sebas12Bot <sebasbote18@gmail.com>
2025-01-28 21:33:51 -0500

    Adicion de autenticaciones y middleware

--- src/router.ts
+++ src/router.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express'
 import { body } from 'express-validator'
-import { createAccount } from './handlers'
+import { createAccount, login } from './handlers'
+import { handleInputErrors } from './middleware/validation'
 
 const router = Router()
 
@@ -20,6 +21,19 @@ router.post('/auth/register',
     body('password')
         .isLength({ min: 8 })
         .withMessage('La contraseña debe tener al menos 8 caracteres'),
-    createAccount)
+    handleInputErrors,
+    createAccount
+)
+
+router.post('/auth/login',
+    body('email')
+        .isEmail()
+        .withMessage('El email no es valido'),
+    body('password')
+        .notEmpty()
+        .withMessage('La contraseña es obligatoria'),
+    handleInputErrors,
+    login
+)
 
 export default router
--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -4,3 +4,8 @@ export const hashPassword = async (password : string) => {
     const salt = await bcrypt.genSalt(10)
     return await bcrypt.hash(password, salt)
 }
+
+export const comparePassword = async (enterPassword : string, hash : string) => {
+    const result = await bcrypt.compare(enterPassword, hash)
+    return result
+}
